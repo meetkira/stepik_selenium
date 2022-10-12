0b40cbd5a210d0a51532a32d167aa9952359e56a
Кристина Ратник <kristi.2001@yandex.ru>
2022-10-12 15:30:16 +0300

    add section 1 files

---
+++ section_1/test_1_6_checkbox_and_radiobutton.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+link = "https://suninjuly.github.io/math.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x = browser.find_element(By.ID, "input_value")
+    result = calc(x.text)
+    input_answer = browser.find_element(By.ID, "answer")
+    input_answer.send_keys(result)
+    option1 = browser.find_element(By.CSS_SELECTOR, "[for='robotCheckbox']")
+    option1.click()
+    option2 = browser.find_element(By.CSS_SELECTOR, "[for='robotsRule']")
+    option2.click()
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+except Exception as error:
+    print(f'Произошла ошибка, вот её трэйсбэк: {error}')
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
---
+++ section_1/test_1_6_find_link.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+import time
+
+link = "http://suninjuly.github.io/find_link_text"
+hidden_link = str(math.ceil(math.pow(math.pi, math.e) * 10000))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    new_link = browser.find_element(By.LINK_TEXT, hidden_link)
+    new_link.click()
+
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+except Exception as error:
+    print(f'Произошла ошибка, вот её трэйсбэк: {error}')
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
